373e8f3e1a9a2a44b3bad421c168f6edcb771364
Joseph Ross <josephross@josephs-mini.socal.rr.com>
2017-01-07 17:02:05 -0800

    Setting up the groundwork.

---
+++ Ping/PGHomeVC.h
@@ -0,0 +1,13 @@
+//
+//  PGHomeVC.h
+//  Ping
+//
+//  Created by Joseph Ross on 2017-01-07.
+//  Copyright © 2017 Joseph Ross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGHomeVC : UIViewController
+
+@end
