c6c2f842d1536eb2e7fc9b5db37bfe5004a56c28
cupojoro <joseph.ross@msn.com>
2017-03-15 09:31:31 -0700

    No longer in use
    replaced by MVC style of MapInterface

--- Ping/PGMapInterfaceVC.m
+++ Ping/PGMapInterfaceVC.m
@@ -5,7 +5,6 @@
 //  Created by Joseph Ross on 2017-01-08.
 //  Copyright © 2017 Joseph Ross. All rights reserved.
 //
-
 #import "PGMapInterfaceVC.h"
 #import "PGGridCell.h"
 #import "PGFilterView.h"
